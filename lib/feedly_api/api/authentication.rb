ebe8df116bbd58096293b154861487949a8b484b
ミューズ <ivan.bolgar@gmail.com>
2014-06-17 02:15:09 +0300

    add boilerplate methods and specs for Authentication API

--- lib/feedly_api/api/authentication.rb
+++ lib/feedly_api/api/authentication.rb
@@ -1,2 +1,13 @@
 module FeedlyApi::API::Authentication
+  def auth
+  end
+
+  def refresh_tokens
+  end
+
+  def obtain_new_access_token
+  end
+
+  def revoke_refresh_token
+  end
 end
---
+++ spec/lib/feedly_api/api/authentication_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe FeedlyApi::API::Authentication do
+end
